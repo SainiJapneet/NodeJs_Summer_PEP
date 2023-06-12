2af94df84fc59708f9c4b15f276444bf9f3669de
japneet <japneetsaini084@gmail.com>
2023-06-12 10:54:38 +0530

    html file loading on server

---
+++ myModule.js
@@ -0,0 +1,6 @@
+exports.sayHello = function (){
+    return "Hello, How are you?";
+}
+exports.getDate = function (){
+    return Date();
+}
---
+++ myModule1.js
@@ -0,0 +1,6 @@
+exports.welcomeMsg = function (){
+    return "Welcome! Good to see you"
+};
+exports.wishMsg = function (){
+    return "Greetings for the day"
+};
